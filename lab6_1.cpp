a456b77a51414571e2ad14deb37564a5db593948
= <=thiranat_kakanmee@cmu.ac.th>
2022-12-09 15:16:19 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,18 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+    int odd=0,even=0,num=1;
+    while(num != 0){
+        cout << "Enter an integer: ";
+        cin >> num;
+        if(num%2==0 && num!=0)
+        {   
+            even++;
+        }else if(num!=0){
+            odd++;
+        }
+    }
+    cout << "#Even numbers = " << even << endl;
+    cout << "#Odd numbers = " << odd << endl;
     return 0;
 }
